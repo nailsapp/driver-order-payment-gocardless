2a1710ee828d29860ae4fd54e45fb281ef78c85b
Pablo de la Peña <p@shedcollective.org>
2015-11-18 17:48:40 +0000

    [WIP] Work on Driver

--- src/GoCardless.php
+++ src/GoCardless.php
@@ -1,7 +1,59 @@
 <?php
 
+/**
+ * GoCardless payment Driver
+ *
+ * @package     Nails
+ * @subpackage  driver-order-payment-gocardless
+ * @category    Driver
+ * @author      Nails Dev Team
+ * @link
+ */
+
 namespace Nails\OrderPayment\Driver;
 
-class GoCardless implements \Nails\OrderPayment\Interfaces\Driver
+use Nails\OrderPayment\Driver\Base;
+
+class GoCardless extends Base
 {
+    protected $sLabel = 'GoCardless';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the driver's configurable options
+     * @return array
+     */
+    public function getConfig(){
+        return array();
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Configures the driver using the saved values from getConfig();
+     */
+    public function setConfig($aConfig){
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Take a payment
+     * @return boolean
+     */
+    public function charge(){
+        return true;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Issue a refund for a payment
+     * @return boolean
+     */
+    public function refund(){
+        return true;
+    }
 }
